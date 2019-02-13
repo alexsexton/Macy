45d196b35dd7922ada419d149709ad8b37cf60dc
Alex Sexton <alex@threefour.co.uk>
2019-02-13 21:13:32 +0000

    Refactor footer

--- footer.php
+++ footer.php
@@ -1,9 +1,8 @@
-<footer><p class="powered-by">
-  <a href="<?php echo apply_filters( 'macy_powered_by_link', esc_url( 'https://wordpress.org' ) ); ?>"><?php echo apply_filters( 'macy_powered_by_text', esc_html__( 'Proudly Powered by WordPress', 'macy' ) ); ?></a></p>
-  
+<footer>
+
+  <a href="<?php echo apply_filters( 'macy_powered_by_link', esc_url( 'https://wordpress.org' ) ); ?>"><?php echo apply_filters( 'macy_powered_by_text', esc_html__( 'Proudly Powered by WordPress', 'macy' ) ); ?></a>
 
 </footer>
-
 
 <?php wp_footer(); ?>
 
