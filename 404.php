c783d21a7fd09c810ffa64688399ccee3411fa64
Alex Sexton <alex@threefour.co.uk>
2019-02-19 17:50:07 +0000

    Remove loop from around content

--- 404.php
+++ 404.php
@@ -1,21 +1,19 @@
 <?php get_header() ?>
-<?php get_template_part('partials/header') ?>
+<?php get_template_part('partials/site-header') ?>
 
 <main class="main">
-  <?php if( have_posts() ): while( have_posts() ): the_post(); ?>
-    <section>
+  <section>
 
-      <header class="the-title">
-        <h1>Page not found</h1>
-      </header>
+    <header class="the-title">
+      <h1>Page not found</h1>
+    </header>
 
-      <article class="the-content">
-        <p>If you entered a web address please check it was correct.</p>
-        <p>You can also <a href="/search">search</a> or <a href="/">browse from the homepage</a> to find the information you need.</p>
-      </article>
+    <article class="the-content">
+      <p>If you entered a web address please check it was correct.</p>
+      <p>You can also <a href="/search">search</a> or <a href="/">browse from the homepage</a> to find the information you need.</p>
+    </article>
 
-    </section>
-  <?php endwhile; endif;?>
+  </section>
 </main>
 
 <?php get_footer() ?>
