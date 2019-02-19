491a555c3f229e25afe68bc3b221051e35f27344
Alex Sexton <alex@threefour.co.uk>
2019-02-19 17:29:00 +0000

    Removed crap from footer

--- footer.php
+++ footer.php
@@ -1,6 +1,5 @@
 <footer>
 
-  <a href="<?php echo apply_filters( 'macy_powered_by_link', esc_url( 'https://wordpress.org' ) ); ?>"><?php echo apply_filters( 'macy_powered_by_text', esc_html__( 'Proudly Powered by WordPress', 'macy' ) ); ?></a>
 
 </footer>
 
