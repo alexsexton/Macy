05020724eee089bb527a5f0c5bc54ee0e79c4634
Alex Sexton <alex@threefour.co.uk>
2019-02-19 17:50:22 +0000

    Added footer nav walker

--- footer.php
+++ footer.php
@@ -1,5 +1,14 @@
 <footer>
 
+  <?php
+    wp_nav_menu ( array (
+      'theme_location' => 'site_nav_footer',
+      'container' => 'ul',
+      'container_id' => '',
+      'depth' => 2,
+      'walker' => new Macy_Nav_Walker(),
+    ));
+  ?>
 
 </footer>
 
