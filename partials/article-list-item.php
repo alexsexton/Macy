ec885beecd7f8810d516ad96cb17fc71401ed355
Alex Sexton <alex@threefour.co.uk>
2019-03-01 19:25:59 +0000

    Refactor article-list partial

--- partials/article-list-item.php
+++ partials/article-list-item.php
@@ -1,17 +1,17 @@
-<article <?php post_class('the-content rich-text'); ?>>
-  <header>
-    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-    <?php get_template_part('partials/entry-meta'); ?>
-  </header>
+<article>
+  <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 
   <?php if (has_post_thumbnail()) : ?>
     <div class="post-thumbnail">
-      <?php the_post_thumbnail('medium'); ?>
+      <?php if (post_type_exists('articles')): ?>
+        <?php the_post_thumbnail('portfolio'); ?>
+        <?php else: ?>
+        <?php the_post_thumbnail('medium_large'); ?>
+      <?php endif; ?>
     </div>
   <?php endif; ?>
 
-  <div class="excerpt">
+  <div class="the-excerpt rich-text">
     <?php the_excerpt(); ?>
   </div>
-
 </article>
