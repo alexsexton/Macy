cd35b1f1682b0d26ef1114ee8728b3a0cae31b2d
Alex Sexton <alex@threefour.co.uk>
2019-03-04 16:44:28 +0000

    refactor articles list

--- partials/article-list-item.php
+++ partials/article-list-item.php
@@ -1,13 +1,15 @@
-<article>
+<article class="list-item">
   <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 
   <?php if (has_post_thumbnail()) : ?>
     <div class="post-thumbnail">
+      <a href="<?php the_permalink(); ?>">
       <?php if (post_type_exists('articles')): ?>
         <?php the_post_thumbnail('portfolio'); ?>
         <?php else: ?>
         <?php the_post_thumbnail('medium_large'); ?>
       <?php endif; ?>
+      </a>
     </div>
   <?php endif; ?>
 
