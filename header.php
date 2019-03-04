d24a70a4862edc4cd31dbb1995e9c739a49e87d5
Alex Sexton <alex@threefour.co.uk>
2019-03-04 16:45:07 +0000

    update icon pathss
    todo: add icons

--- header.php
+++ header.php
@@ -5,11 +5,11 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/images/favicon.png" />
-  <link rel="apple-touch-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/images/apple-touch-icon.png" />
-  <link rel="apple-touch-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/images/apple-touch-icon-76x76.png" sizes="76x76" />
-  <link rel="apple-touch-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/images/apple-touch-icon-120x120.png" sizes="120x120" />
-  <link rel="apple-touch-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/images/apple-touch-icon-152x152.png" sizes="152x152" />
+  <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/static/images/favicon.png" />
+  <link rel="apple-touch-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/static/images/apple-touch-icon.png" />
+  <link rel="apple-touch-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/static/images/apple-touch-icon-76x76.png" sizes="76x76" />
+  <link rel="apple-touch-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/static/images/apple-touch-icon-120x120.png" sizes="120x120" />
+  <link rel="apple-touch-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/static/images/apple-touch-icon-152x152.png" sizes="152x152" />
 
   <?php wp_head(); ?>
 
