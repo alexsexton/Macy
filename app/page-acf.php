35b50bc428352ce4512a27045670f24356ed066a
Alex Sexton <alex@threefour.co.uk>
2019-03-26 12:08:34 +0100

    Added page custom fields

---
+++ app/page-acf.php
@@ -0,0 +1,52 @@
+<?php
+if( function_exists('acf_add_local_field_group') ):
+
+acf_add_local_field_group(array(
+	'key' => 'group_5c913a7cadb67',
+	'title' => 'Page Options',
+	'fields' => array(
+		array(
+			'key' => 'field_5c913a825cd85',
+			'label' => 'Page Background Image',
+			'name' => 'page_background_image',
+			'type' => 'image',
+			'instructions' => 'For best restults, create this image at 1024px wide and 1365px tall.',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'return_format' => 'array',
+			'preview_size' => 'page-image-preview',
+			'library' => 'all',
+			'min_width' => '',
+			'min_height' => '',
+			'min_size' => '',
+			'max_width' => '',
+			'max_height' => '',
+			'max_size' => '',
+			'mime_types' => '',
+		),
+	),
+	'location' => array(
+		array(
+			array(
+				'param' => 'page',
+				'operator' => '==',
+				'value' => '63',
+			),
+		),
+	),
+	'menu_order' => 0,
+	'position' => 'acf_after_title',
+	'style' => 'default',
+	'label_placement' => 'top',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => '',
+	'active' => true,
+	'description' => '',
+));
+
+endif;
